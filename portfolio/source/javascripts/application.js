a22a818cfe12111a7c2888dc2ce6e1d2ebcde253
Pavel Liser <liser.pavel@gmail.com>
2018-08-23 15:29:30 +0300

    made typed.js work in html script

--- portfolio/source/javascripts/application.js
+++ portfolio/source/javascripts/application.js
@@ -1,3 +1,13 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+// import Typed from 'typed.js';
+
+// const typed = new Typed('#typer', {
+//   strings: ["First sentence.", "Second sentence."],
+//   typeSpeed: 30
+// });
+
+console.log("Hello from application.js");
+
